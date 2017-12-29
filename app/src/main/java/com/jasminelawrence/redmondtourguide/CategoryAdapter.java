a48b48725fc1fa87cf08bac82bd330fb3c46b186
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2017-12-28 15:35:18 -1000

    Inital commit with working sliding tabs

---
+++ app/src/main/java/com/jasminelawrence/redmondtourguide/CategoryAdapter.java
@@ -0,0 +1,44 @@
+package com.jasminelawrence.redmondtourguide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Jasmine on 12/28/17.
+ */
+
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private String tabTitles[] = new String[] { "Live", "Work", "Eat" , "Explore" };
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new TestFragment();
+        } else if (position == 1){
+            return new TestFragment();
+        }else if (position == 2){
+            return new TestFragment();
+        } else {
+            return new TestFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        return tabTitles[position];
+
+    }
+}
